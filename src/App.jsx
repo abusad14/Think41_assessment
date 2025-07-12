1bfb981839281c668bffdc0a8e55b849bdeb0783
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-07-12 12:34:25 +0530

    New line

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h1 className="text-red-800">Home ....</h1>
       <h2>This is practice file..</h2>
       <h2>This is practice file..</h2>
+      <h2>This line is added</h2>
     </div>
   );
 };
