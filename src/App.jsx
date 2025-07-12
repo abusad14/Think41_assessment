3529343f43021fd3f3c2f0cf2f35238846fbb689
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-07-12 12:25:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-800">Home ....</h1>
+      <h2>This is practice file..</h2>
+    </div>
+  );
+};
+
+export default App;
