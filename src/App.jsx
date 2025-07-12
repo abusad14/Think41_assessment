1d92707d9f3c2427737860f70de0ef7851cb1a07
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-07-12 14:02:55 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [url, setUrl] = useState("");
+  const [urlList,setUrlList] = useState([]);
+  const [latestUrl,setLatestUrl] = useState(null);
+
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const trimmedUrl = url.trim();
+    if(trimmedUrl === "") return;
+    const alreadyExists = urlList.some((u)=>{u.toLowerCase()===trimmedUrl.toLowerCase();});
+    let updatedList = [...urlList]
+    if(!alreadyExists){
+       updatedList = [trimmedUrl, ...urlList];
+      // setUrlList(updatedList);
+      // setLatestUrl(trimmedUrl)
+      // console.log(`Visited URL : ${trimmedUrl}`);
+    }
+    updatedList = updatedList.slice(0,10)
+    setUrlList(updatedList);
+      setLatestUrl(trimmedUrl)
+    setUrl("");
+    // console.log(url)
+  }
   return (
     <div>
-      <h1 className="text-red-800">Home ....</h1>
-      <h2>This is practice file..</h2>
-      <h2>This is practice file..</h2>
-      <h2>This line is added</h2>
-      <h2>This line is addedddddddddddd</h2>
+      <form onSubmit={handleSubmit}>
+        <input className="border-1 m-2" type="text" name="url" value={url} onChange={(e)=>{setUrl(e.target.value)}} />
+        <button className="border-1 m-2 p-2" type="submit">Visit URL</button>
+      </form>
+      {latestUrl && (
+        <div>
+          <h3>Recent visited Url</h3>
+          <p>{latestUrl}</p>
+        </div>
+      )}
+      <h3>Most recent 10 URLs </h3>
+      <ul>
+        {urlList.map((u,i)=>{
+          <li key={i}>{u}</li>
+        })}
+      </ul>
+
     </div>
   );
 };
